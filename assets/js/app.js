440778a3601b4f9f165cc633da06504719ae2bd5
Tara Z. Manicsic <tara@modulus.io>
2015-05-25 22:27:05 -0400

    Updates files and fixes FF bug.
    Updates jquery and switches to hosted file.
    Switches to minified version of simpleCart.
    Updates juliannie picture.
    Fixes errors in app.js file to work in FF.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,26 +1,14 @@
-simpleCart({
-  checkout: {
-    type: "PayPal",
-    email: "youarewhatyouart@gmail.com"
-  },
-  currency: "USD"
+$(document).ready(function () {
+  simpleCart({
+    checkout: {
+      type: "PayPal",
+      email: "youarewhatyouart@gmail.com"
+    },
+    currency: "USD"
+  });
+
+  $(".basket-menu").click(function (event) {
+    event.preventDefault();
+    $("header").toggleClass("show-basket");
+  });
 });
-
-$( ".basket-menu" ).click(function() {
-  event.preventDefault();
-  $("header").toggleClass( "show-basket" );
-});
-
-// var gift  = document.getElementsByClassName("gift");
-// console.log('gift', gift);
-
-// var addGift = function () {
-//   var name = this.getAttribute("name");
-//   var price = this.getAttribute("price");
-//   console.log(name + ' $' + price);
-// };
-
-// for (var i = 0; i < gift.length; i++) {
-//   gift[i].addEventListener('click', addGift, false);
-// }
-
